f9928df462cef96a24f6d3ae5daaec027a3b5ef0
Donghyun Kim <91539013+kdh3543@users.noreply.github.com.>
2024-05-07 17:38:53 +0900

    feat: search 기능 추가 중 & library 기능 추가

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,6 +5,7 @@ import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
 import { Song } from '@/types';
+import MediaItem from './MediaItem';
 
 interface LibraryProps {
   songs: Song[];
@@ -34,7 +35,11 @@ const Library = ({ songs }: LibraryProps) => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs?.map((item) => (
+          <MediaItem onClick={() => {}} key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 };
